6dae37b8f1ca15d6210402482184fe45ace8bd01
andreasadinata <andreasadinata@hotmail.com>
2017-03-06 23:49:13 -0800

    Final Touch

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 $(document).ready(function () {
-    $('.js-result').show();
+    $('.js-result').hide();
+    $('#js-back').hide();
+
     $('.js-dj').click(function () {
         var value = $(this).text(); //get text value from the click
         getArtistIdByAtristName(value);
         $('.js-result').show();
         $('.js-content').hide();
+        $('#js-back').show();
     });
-});
 
+    $('#js-back').click(function () {
+        $('.js-result').hide();
+        $('#js-back').hide();
+        $('.js-content').show();
+
+    });
+
+});
 
 function getArtistIdByAtristName(artistName) {
     /* Update all the parameters for your API test*/
@@ -27,7 +37,7 @@ function getArtistIdByAtristName(artistName) {
         /* if the call is successful (status 200 OK) show results */
         .done(function (result) {
             /* if the results are meeningful, we can just console.log them */
-            console.log(result);
+
             var artistID = result.artists.items[0].id;
             getRelatedTracksByArtistID(artistID);
         })
@@ -41,7 +51,7 @@ function getArtistIdByAtristName(artistName) {
 }
 
 function getRelatedTracksByArtistID(artistID) {
-    console.log(artistID);
+
     /* Update all the parameters for your API test*/
     var result = $.ajax({
             /* update API end point */
@@ -65,9 +75,6 @@ function getRelatedTracksByArtistID(artistID) {
 }
 
 function displaySearchResults(relatedTracks) {
-
-    console.log(relatedTracks);
-
     //create an empty variable to store one LI for each one the results
     var buildTheHtmlOutput = "";
 
@@ -85,4 +92,31 @@ function displaySearchResults(relatedTracks) {
 
     //use the HTML output to show it in the index.html
     $(".js-result ul").html(buildTheHtmlOutput);
+}
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
